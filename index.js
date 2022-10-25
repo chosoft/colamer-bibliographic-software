8c693d1569637ae08e5cf1177b412c684dcffb17
chosoft <creadorcuentasmolina@gmail.com>
2022-10-24 20:36:45 -0500

    Funcion de router creada. Pendiente configurar helmet para trabajar con archivos CDN. Configurar la carpeta de auth para empezar a trabajar las sesiones de usuarios. Ademas de configurar la Base de Datos(Pendiente de eleccion)

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const server = express()
-const { port } = require('./configs/env')
+const router = require('./routes/router')
 const helmet = require('helmet')
+const path = require('path')
+const { port } = require('./configs/env')
 
+
+server.use(express.static(path.join(__dirname,'public')))
 server.use(express.json())
+server.use(express.urlencoded({ extended:true }))
 server.use(helmet())
+
+server.disable('x-powered-by')
+
+server.use(router)
 server.listen(port, () => {
     console.log(`SERVER IS RUNNING http://localhost:8080/`)
 })
---
+++ routes/router.js
@@ -0,0 +1,6 @@
+const router = (server) => {
+//Put the routes and his router inside this
+//Ex: server.use('/ruta',router)
+}
+
+module.exports = router
