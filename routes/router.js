2f52d3ba594a05e2252a23cf436f46188f331171
XDiego978 <diegogamer3473@gmail.com>
2022-10-28 15:45:21 -0500

    Subiendo Proyecto Modificado

--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,12 @@
 const index = require('./home/index')
 const signIn = require('./sign-in/sign-in')
+const books = require('./books/books')
 const router = (server) => {
     //Put the routes and his router inside this
     //Ex: server.use('/ruta',router)
     server.use('/',index)
     server.use('/sign-in',signIn)
+    server.use('/books', books)   
 }
 
 module.exports = router
