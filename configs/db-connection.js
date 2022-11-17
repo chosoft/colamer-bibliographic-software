23b61fa6e4afe45d9754cd0a59eb341ebae72e18
XDiego978 <diegogamer3473@gmail.com>
2022-11-17 09:44:51 -0500

    Configuracion de los libros

--- configs/db-connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const { db_book_user, db_book_password, db_book_name, db_book_host } = require('./env');
-
-const URI = `mongodb+srv://${db_book_user}:${db_book_password}@${db_book_host}${db_book_name}?retryWrites=true&w=majority`
-
-mongoose.connect(URI)
-.then(() => console.log('Connected to Database'))
-.catch((error) => (error));
--- models/books.js
+++ models/books.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose')
 const { Schema,model } = mongoose
-const bcrypt = require('bcrypt')
 
 const BOOK_SCHEMA = new Schema({
-    tittle:String,
-    autor:String,
+    title:String,
+    author:String,
+    signature:String,
+    barcode:String,
+    collection:String,
+    copies:Number,
+    available:Boolean,
+    img:String
 })
