853186c9bb28fe61affaa98eff7511c0fc7a5b59
chosoft <creadorcuentasmolina@gmail.com>
2023-02-22 21:40:52 -0500

    Cambios Seccion de usuarios

---
+++ public/js/private/defaults/popUp.js
@@ -0,0 +1,16 @@
+const bg = document.querySelector('.popUpBg')
+const modal = document.querySelector('.modal')
+
+const btnActiver = document.querySelector('.activer')
+const btnCloser = document.querySelector('.closer')
+
+btnActiver.onclick = (e) => {
+    e.preventDefault()
+    bg.style.top = "0"
+    bg.style.opacity = "1"
+}
+btnCloser.onclick = (e) => {
+    e.preventDefault()
+    bg.style.top = "200%"
+    bg.style.opacity = "0"
+}
