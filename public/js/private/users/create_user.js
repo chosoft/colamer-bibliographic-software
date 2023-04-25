450860c994f094ae16143936c693ef44db35125d
chosoft <creadorcuentasmolina@gmail.com>
2023-04-25 14:10:05 -0500

    Subiendo comentarios para el JS de crear usuarios

--- public/js/private/users/create_user.js
+++ public/js/private/users/create_user.js
@@ -1,40 +1,71 @@
+//Main Button Element
 const createUserBtn = document.querySelector('.submitBtn')
-
+//Retrieve all fields of the form
 const fields = document.querySelectorAll('.field')
-
+//This is a event function that allow update the field after try to upload the form without any value
+//this dont work for the password field
 const fieldRecover = (e) => {
+    //Retrieve the input element of the event 
     const field = e.target
-    const keyId = field.id
+    //The id is using to retrieve other elements that belongs to the input
+    const keyId = field.id //Retrieve the id tag of the element
+    //Retrieve the content in which the error was displayed
     const posibleErrorElement = document.querySelector(`#${keyId}PosibleError`)
+    //Retrieve the label in which the error was displayed
     const label = document.querySelector(`#${keyId}Label`)
+    //Delete the text inside the error element
     posibleErrorElement.innerText = ''
+    //Change the color of the label
     label.style.color = "#4180E2"
+    //Change the border color of the input
     field.style.border = "2px solid transparent"
 }
-
+//This function have two task
+//The first is update the field components after trying to submit it empty
+//The second is check that the value of the input has all the necesary features to considerate a valid password
+//all this process happend at the moment in which the user press any key inside the input
 const passwordRecover = (e) => {
+    //PSDT: I've the task of change this part of code because its repetetive(I need use the DRY method)
+    //Retrieve the pwd field
     const pwdField = e.target
-    const fieldId = pwdField.id
+    //Retrieve the ID
+    const fieldId = pwdField.id //This id allow the work with other elements that belongs to this input
+    //Retrieve the error element
     const posibleErrorElement = document.querySelector(`#${fieldId}PosibleError`)
+    //Retrieve the label element
     const labelPwdElemnt = document.querySelector(`#${fieldId}Label`)
+    //Change the label font color
     labelPwdElemnt.style.color = "#4180E2"
+    //Delete the text inside the error element
     posibleErrorElement.innerText = ''
+    //Change the border color of the field
     pwdField.style.border = "2px solid transparent"
+    //This is the process to chechk the necesary features to consider the password a valid password
 
+    //This object contains all the REGEX that check each feature of the password
     const featuresToCheck = {digit: /[0-9]/, uppercase: /[A-Z]/, characters: /^[a-zA-Z0-9]{10,30}$/}
+    //Retrieve the value of the password
     const value = pwdField.value
+    //This loop will check each feature and will test it using the REGEX inside of them
     for (const key in featuresToCheck) {
+        //Key refers to the name of the feature that is processing
+        //This extract the regex of the feature
         const featureRegex = featuresToCheck[key]
+        //This retrieve the element of the feature list using the key
         const elementToChange = document.querySelector(`#${key}Feature`)
+        //Check if the feature is in the value of the input
         if(featureRegex.test(value)){
+            //If the feature is in the value it will change the color(to green) of the element that represent in the list
             elementToChange.style.color = "#48e015"
         }else{
+            //Otherwise the color of the element in the list will be red
             elementToChange.style.color = "#FA2828"
         }
     }
 }
-
+//This loop asing to each field the update function
 for (const field of fields) {
+    //If the field is the password field it assing the right function to that field
     if(field.id === 'password'){
         field.onkeyup = passwordRecover
     }else{
