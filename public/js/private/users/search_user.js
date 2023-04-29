8dcf0d418e9f260fd6b4cfbb86ed79f27647365f
chosoft <creadorcuentasmolina@gmail.com>
2023-04-29 17:50:33 -0500

    Finalizada la parte de seleccion de filtros en el frontend, momento de organizar y corregir el codigo, ademas tengo que corregir el error en la seccion de buscar mediante

--- public/js/private/users/search_user.js
+++ public/js/private/users/search_user.js
@@ -30,7 +30,6 @@ const selectSearchMode = (e) => {
             tag.style.position = 'relative'
             tagsCont.attributes.isopen.value = 'false'
             const hiddenTags = Array.from(searchModeTags).filter(tagElmt => tagElmt!=tag)
-            console.log(hiddenTags)
             for (const hiddenTag of hiddenTags) {
                 hiddenTag.attributes.active.value = 'false'
                 hiddenTag.style.display = 'none'
@@ -41,8 +40,7 @@ const selectSearchMode = (e) => {
         }
         
     } catch (error) {
-        alert('error')
-        console.log(error)
+
     }
 }
 
@@ -50,7 +48,64 @@ for (const tag of searchModeTags) {
     tag.onclick = selectSearchMode
 }
 
+//Codigo para la parte de filtrar por
+const addFilter = document.querySelector('#addFilter')
 
+const toggleFilterView = (e) => {
+    e.preventDefault()
+    const avaibleOpts = Array.from(document.querySelectorAll('.optFilter')).filter(opt => !(JSON.parse(opt.attributes.selected.value)))
+    const optsCont = document.querySelector('.addOpts')
+    if(!avaibleOpts.length){
+        addFilter.attributes.isselecting.value = 'false'
+        optsCont.style.display = 'none'
+        return
+    }
+    const isSelecting = JSON.parse(addFilter.attributes.isselecting.value)
+
+    if(!isSelecting){
+        addFilter.attributes.isselecting.value = 'true'
+        optsCont.style.display = 'flex'
+        return
+    }
+    addFilter.attributes.isselecting.value = 'false'
+    optsCont.style.display = 'none'
+}
+
+addFilter.onclick = toggleFilterView
+
+const allDeleteFilterBtns = document.querySelectorAll('.deleteOpt')
+
+const toggleFilterSelecteds = (e) =>{
+    e.preventDefault()
+    const idWord = e.target.attributes.idWord.value
+    const optToHide = document.querySelector(`#${idWord}Opt`)
+    const btnToDisplay = document.querySelector(`#${idWord}Filter`)
+    btnToDisplay.attributes.selected.value = 'false'
+    optToHide.attributes.selected.value = 'false'
+}
+
+for (const deleteBtn of allDeleteFilterBtns) {
+    deleteBtn.onclick = toggleFilterSelecteds
+}
+
+
+const allFilterBtns = document.querySelectorAll('.optFilter')
+
+const toggleSelectedState = (e) => {
+    e.preventDefault()
+    const filtersCont = document.querySelector('#addFilter')
+    const idWord = e.target.attributes.idWord.value
+    const btnClicked = document.querySelector(`#${idWord}Filter`)
+    const optToChange = document.querySelector(`#${idWord}Opt`)
+    btnClicked.attributes.selected.value = 'true'
+    optToChange.attributes.selected.value = 'true'
+    filtersCont.click()
+
+}
+
+for (const btn of allFilterBtns) {
+    btn.onclick = toggleSelectedState
+}
 
 const searchBar = document.querySelector('.userSearchbar')
 
