2b3c5cf0930de779990344e01af39272fad02c95
XDiego978 <diegogamer3473@gmail.com>
2023-05-01 16:37:44 -0500

    Tratando de hacer errores

--- public/js/private/books/create_book.js
+++ public/js/private/books/create_book.js
@@ -1,7 +1,13 @@
+// Variables that i want to use
 const saveBook = document.querySelector(".submitBtn")
+var input = document.querySelector('.link');
+var image = document.querySelector('.image');
 
+// Fuction to create the book
 saveBook.addEventListener('click', (e) => {
+    // Reset de configs of de event
     e.preventDefault()
+    // Declare the info of the input
     const info = {
         title: document.querySelector('#tittle').value,
         author: document.querySelector('#author').value,
@@ -13,30 +19,52 @@ saveBook.addEventListener('click', (e) => {
         borrowed: parseInt(document.querySelector('#borrowed').value),
         img: document.querySelector('.link').value,
     };
+    console.log(info.author)
+    // First go through the checker
     Checker(info)
+    // And for last one create the book
     createBook(info)
 })
 
+// Fuction of check the information
 const Checker = async (info) => {
     try {
+        // Save only the copies and borrowed of the variable info
         const { copies, borrowed } = info
+        // Validate if the copies is > than borrowed
         if (copies > borrowed) {
+            // Add the new variable avaible but in this case in true
             info.available = true;
         }
+        // Validate if the copies is <= than borrowed
         else if (copies <= borrowed) {
+            // Add the new variable but in false
             info.available = false;
         }
     } catch (error) {
+        // Print the error
         console.log(error)
     }
 }
 
+// Fuction of create the book
 const createBook = async (info) => {
     try {
+        // Save the answer of the peticion 
         const respuesta = await axios.post(`books`,info)
+        // Print the answer
         console.log(respuesta)
     } catch (error) {
-        console.log(error)
-        console.log("Error en la API")
+        // Print the error
+        const msg = error.response.data.msg
+        console.log(msg)
     }
 }
+
+// Fuction to preview the image
+input.addEventListener('paste', (e) => {
+    // Save the url in a variable
+    var url = e.clipboardData.getData("text")
+    // Change the attribute of the image for the url
+    image.setAttribute('src', url)
+})
