f699f0efab4f4af0e4ce0b9c0f580e5e41445f8f
XDiego978 <diegogamer3473@gmail.com>
2023-05-01 15:25:03 -0500

    Intentando hacer funcionar el img

--- public/js/private/books/test.js
+++ public/js/private/books/test.js
@@ -1,13 +1,8 @@
-var img = document.querySelector('.link')
-var img2 = document.querySelector('.image')
+var input = document.querySelector('.link');
+var image = document.querySelector('.image');
 
-console.log(img.value)
-
-img.addEventListener("onchange", async () => {
-        img2.setAttribute("src", img.value)
-        console.log(img2.src)
-    })
-
-const prueba = (info) => {
-    console.log(info)
-}  
+input.addEventListener('paste', (e) => {
+    var url = e.clipboardData.getData("text")
+    console.log(url)
+    image.setAttribute('src', url)
+})
